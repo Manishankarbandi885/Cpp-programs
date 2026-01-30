abe45a570aed90325c8aa30298c246580ea7725e
Manishankarbandi885 <manishankarbandi885@gmail.com>
2026-01-30 10:29:30 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    	int a,b;
+	cout<<"Enter 2 values "<<endl;
+	cin>>a>>b;
+	cout<<"you entered : "<<endl<<a<<endl <<b<<endl;
+	cout<<"sum = "<<a+b;
+
+
+    return 0;
+}
+//Output
+
+/*Enter 2 values 
+2
+4
+you entered : 
+2
+4
+sum = 6*/
